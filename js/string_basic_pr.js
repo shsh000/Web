e260b71ac1014ab7872aff6975362092c653bd45
ysh <hee@yedam.ac>
2022-06-17 16:20:59 +0900

    20220617

--- js/string_basic_pr.js
+++ js/string_basic_pr.js
@@ -66,3 +66,31 @@
 // console.log(str1.substring(11, 8));
 // console.log(str1.substring(11, -8));
 
+// substr()
+// const str1 = 'This is the only one story';
+// console.log(str1.substr(8,11)); // the only on
+// console.log(str1.substr(11,8)); //  only on
+// console.log(str1.substr(11, -8)); // null
+// console.log(str1.substr(-10, 8)); //  one sto
+// console.log(str1.substr(10)); // 파라미터 값이 하나만 있으면 시작인덱스부터 문자열 끝까지 반환됨
+// console.log(str1.substr(0,-10)); // 끝 인덱스는 마이너스 값을 사용할 수 없음
+
+// toString() 숫자를 문자열로 변환
+// const num1 = 123;
+// const num2 = 123.45;
+// const bool = true;
+// const str = '문자열타입';
+// const arr = [1,2,'a','b',3];
+// const obj = {key: 'data', value:15};
+// console.log(num1.toString()); // 문자열 123
+// console.log(num2.toString()); // 문자열 123.45
+// console.log(bool.toString()); // 문자열 true
+// console.log(str.toString()); // 문자열타입
+// console.log(arr.toString()); // 문자열 1,2,a,b,3
+// console.log(obj.toString()); // 문자열이 아닌 객체만 반환
+// console.log(obj.key.toString()); // key 값인 data 반환
+// console.log(obj.value.toString()); // value 값인 15 반환
+
+// indexOf() 문자열 시작 위치 찾기
+const str1 = 'good morning, good afternoon, good evening, and good night';
+console.log(str1.indexOf)
